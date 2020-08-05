5550d2bd3d1d6dcb436400ed96fc37d9e803d153
Michael Rabon <nobar86@gmail.com>
2020-08-05 02:14:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,12 +14,12 @@ end
 def monopoly_with_second_tier
    monopoly = {railroads: {pieces: 4, rent_in_dollars: {}}}
 
-   } 
+    
 
 end
 
 def monopoly_with_third_tier
-	 monopoly = {railroads: {pieces: 4, rent_in_dollars: {}, names: {}}
+	 monopoly = {railroads: {pieces: 4, rent_in_dollars: {}, names: {}}}}
    
 end
 
